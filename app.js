9e8ed4f16c2e5f6eb15208f8615fe65dbbdbc637
Erdem Köşk <erdemkosk@gmail.com>
2025-06-26 01:51:10 +0300

    Adding new script

--- app.js
+++ app.js
@@ -28,7 +28,7 @@ app.post('/upload', upload.single('pdf'), async (req, res) => {
   }
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 80;
 app.listen(PORT, () => {
   console.log(`Server started on http://localhost:${PORT}`);
 }); 
